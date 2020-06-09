4aece5c81bf83362f4cff9e7af8e31d2b5899921
Nader Nabil <nnabil@youxel.com>
2020-06-09 04:10:04 +0200

    change BaseViewModel calling data structure

--- app/src/main/java/com/architecture/clean/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/architecture/clean/ui/fragment/home/HomeViewModel.kt
@@ -1,11 +1,9 @@
 package com.architecture.clean.ui.fragment.home
 
-import androidx.lifecycle.MutableLiveData
-import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.architecture.clean.domain.model.popular_person.local.PopularPersons
 import com.architecture.clean.domain.model.popular_person.parameters.PopularPersonsRequest
-import com.architecture.clean.domain.model.response.ErrorStatus
 import com.architecture.clean.domain.usecase.popular_persons.GetPopularPersonsUseCase
 import com.architecture.clean.ui.view_model.BaseViewModel
 import javax.inject.Inject
@@ -15,33 +13,13 @@ class HomeViewModel @Inject constructor(
 ) : BaseViewModel() {
     private val TAG = HomeViewModel::class.java.simpleName
 
-    private val popularPersonsData: MutableLiveData<ArrayList<PopularPersons>>
-            by lazy { MutableLiveData<ArrayList<PopularPersons>>() }
+    private val popularPersonsData: MutableLiveData<List<PopularPersons>>
+            by lazy { MutableLiveData<List<PopularPersons>>() }
 
-    val popularPersonsLiveData: LiveData<ArrayList<PopularPersons>> = popularPersonsData
+    val popularPersonsLiveData: LiveData<List<PopularPersons>> = popularPersonsData
     fun getPopularPersons(parameters: PopularPersonsRequest) {
-        getPopularPersonsUseCase.execute(parameters) {
-            isLoading {
-                isloading.value = it
-            }
-            onComplete {
-                Log.d(TAG, it.toString())
-                popularPersonsData.value = it as ArrayList<PopularPersons>
-            }
-
-            onError { throwable ->
-                when {
-                    throwable.errorStatus == ErrorStatus.UNAUTHORIZED -> {
-
-                    }
-                    else -> error.value = throwable
-                }
-
-            }
-
-            onCancel {
-                cancellationMessage.value = it.message
-            }
+        callApi(popularPersonsData) { statesCallBack ->
+            getPopularPersonsUseCase.execute(parameters, statesCallBack)
         }
     }
 
--- app/src/main/java/com/architecture/clean/ui/view_model/BaseViewModel.kt
+++ app/src/main/java/com/architecture/clean/ui/view_model/BaseViewModel.kt
@@ -4,15 +4,39 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.architecture.clean.domain.model.response.ErrorModel
+import com.architecture.clean.domain.model.response.ErrorStatus
+import com.architecture.clean.domain.usecase.base.CompletionBlock
 import javax.inject.Inject
 
-open class BaseViewModel@Inject constructor() : ViewModel() {
-    internal val isloading: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
-    val isLoadingLiveData: MutableLiveData<Boolean> by lazy { isloading }
+open class BaseViewModel @Inject constructor() : ViewModel() {
+    private val isLoading: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val isLoadingLiveData: MutableLiveData<Boolean> by lazy { isLoading }
 
-    internal val error: MutableLiveData<ErrorModel> by lazy { MutableLiveData<ErrorModel>() }
+    private val error: MutableLiveData<ErrorModel> by lazy { MutableLiveData<ErrorModel>() }
     val errorLiveData: LiveData<ErrorModel> by lazy { error }
 
-    internal val cancellationMessage: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    private val cancellationMessage: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val cancellationMsgLiveData: LiveData<String> by lazy { cancellationMessage }
+
+    fun <T> callApi(data: MutableLiveData<T> ,apiCall: (CompletionBlock<T>)->Unit) {
+        apiCall.invoke {
+            isLoading {
+                isLoading.value = it
+            }
+            onComplete {
+                data.value = it
+            }
+            onError { throwable ->
+                when (throwable.errorStatus) {
+                    ErrorStatus.UNAUTHORIZED -> {
+                    }
+                    else -> error.value = throwable
+                }
+
+            }
+            onCancel {
+                cancellationMessage.value = it.message
+            }
+        }
+    }
 }
