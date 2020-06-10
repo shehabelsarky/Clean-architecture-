200de0003fdadf3777e1d6bc62717f150d3f8211
Nader Nabil <nnabil@youxel.com>
2020-06-10 15:34:09 +0200

    change BaseViewModel calling data structure

--- app/src/main/java/com/architecture/clean/ui/view_model/BaseViewModel.kt
+++ app/src/main/java/com/architecture/clean/ui/view_model/BaseViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.architecture.clean.domain.model.response.ErrorModel
-import java.util.concurrent.CancellationException
 import com.architecture.clean.domain.model.response.ErrorStatus
 import com.architecture.clean.domain.usecase.base.CompletionBlock
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.launch
+import java.util.concurrent.CancellationException
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -24,62 +24,60 @@ open class BaseViewModel @Inject constructor() : ViewModel() {
     private val cancellationMessage: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val cancellationMsgLiveData: LiveData<String> by lazy { cancellationMessage }
 
-    fun <T> callApiDirectly(data: MutableLiveData<T>, apiCall: (CompletionBlock<T>) -> Unit) {
-        fun setLoading(isLoading: Boolean) {
-            this.isLoading.value = isLoading
-        }
+    fun setLoading(isLoading: Boolean) {
+        this.isLoading.value = isLoading
+    }
 
-        fun setErrorReason(errorModel: ErrorModel) {
-            error.value = errorModel
-        }
+    fun setErrorReason(errorModel: ErrorModel) {
+        error.value = errorModel
+    }
 
-        fun setCancellationReason(cancellationException: CancellationException) {
-            cancellationMessage.value = cancellationException.message
-        }
+    fun setCancellationReason(cancellationException: CancellationException) {
+        cancellationMessage.value = cancellationException.message
+    }
 
-        fun <T> callApi(data: MutableLiveData<T>, apiCall: (CompletionBlock<T>) -> Unit) {
-            apiCall.invoke {
-                isLoading {
-                    isLoading.value = it
-                }
-                onComplete {
-                    data.value = it
-                }
-                onError { throwable ->
-                    when (throwable.errorStatus) {
-                        ErrorStatus.UNAUTHORIZED -> {
-                        }
-                        else -> error.value = throwable
+    fun <T> callApi(data: MutableLiveData<T>, apiCall: (CompletionBlock<T>) -> Unit) {
+        apiCall.invoke {
+            isLoading {
+                isLoading.value = it
+            }
+            onComplete {
+                data.value = it
+            }
+            onError { throwable ->
+                when (throwable.errorStatus) {
+                    ErrorStatus.UNAUTHORIZED -> {
                     }
+                    else -> error.value = throwable
+                }
 
-                }
-                onCancel {
-                    cancellationMessage.value = it.message
-                }
+            }
+            onCancel {
+                cancellationMessage.value = it.message
             }
         }
+    }
 
-        fun <T> callApi(data: ConflatedBroadcastChannel<T>, apiCall: (CompletionBlock<T>) -> Unit) {
-            apiCall.invoke {
-                isLoading {
-                    isLoading.value = it
+    fun <T> callApi(data: ConflatedBroadcastChannel<T>, apiCall: (CompletionBlock<T>) -> Unit) {
+        apiCall.invoke {
+            isLoading {
+                isLoading.value = it
+            }
+            onComplete {
+                viewModelScope.launch {
+                    data.offer(it)
                 }
-                onComplete {
-                    viewModelScope.launch {
-                        data.offer(it)
+            }
+            onError { throwable ->
+                when (throwable.errorStatus) {
+                    ErrorStatus.UNAUTHORIZED -> {
                     }
+                    else -> error.value = throwable
                 }
-                onError { throwable ->
-                    when (throwable.errorStatus) {
-                        ErrorStatus.UNAUTHORIZED -> {
-                        }
-                        else -> error.value = throwable
-                    }
 
-                }
-                onCancel {
-                    cancellationMessage.value = it.message
-                }
+            }
+            onCancel {
+                cancellationMessage.value = it.message
             }
         }
     }
