939850c0ba93122df8d07eb78bcd09984212a3ba
shehabelsarky <nnabil.c@channels.com.sa>
2020-06-15 19:27:48 +0200

    config

--- data/src/main/java/com/examples/data/restful/Config.kt
+++ data/src/main/java/com/examples/data/restful/Config.kt
@@ -1,7 +1,7 @@
 package com.examples.data.restful
 
 object Config {
-    const val API_KEY = ""
+    const val API_KEY = "aede46592fa18e618a51016ae6036c11"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_IMAGE_URL="http://image.tmdb.org/t/p/w342"
     const val POPULAR_ACTORS = "person/popular"
