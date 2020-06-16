e7c20a0ec19eb7e2c07b4b771eee4f70ca51cbeb
shehabelsarky <nnabil.c@channels.com.sa>
2020-06-16 17:42:44 +0200

    adding kotlin DSL to multi modules

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,9 +1,9 @@
 object Versions {
     val core_ktx_version = "1.3.0"
-    val kotlin_version =  "1.3.41"
-    val support_version =  "29.0.0"
-    val picasso_version =  "2.5.2"
-    val arch_version =  "1.1.1"
+    val kotlin_version = "1.3.41"
+    val support_version = "29.0.0"
+    val picasso_version = "2.5.2"
+    val arch_version = "1.1.1"
     val daggerVersion = "2.16"
     val nav_version = "2.3.0-beta01"
     val appcompat_version = "1.1.0"
@@ -11,7 +11,7 @@ object Versions {
     val cardview_version = "1.0.0"
     val recyclerview_version = "1.1.0-alpha01"
     val multidex_version = "2.0.1"
-    val lifecycle_version =  "2.2.0"
+    val lifecycle_version = "2.2.0"
     val legacy_version = "1.0.0"
     val room_version = "2.2.5"
     val room_coroutines_version = "2.1.0-alpha03"
@@ -36,6 +36,22 @@ object Versions {
     val mockitoKotlinVersion = "1.5.0"
 }
 
+
+object Android {
+    val applicationId = "com.cooking.coroutine"
+    val compileSdkVersion = 29
+    val buildToolsVersion = "29.0.2"
+    val minSdkVersion = 23
+    val targetSdkVersion = 29
+    val versionCode = 1
+    val versionName = "1.0"
+
+    val kotlin_version = "1.3.72"
+    val mockitoKotlinVersion = "1.5.0"
+    val nav_version = "2.1.0"
+    val arch_version = "1.1.1"
+}
+
 object Kotlin {
     val kotlin_stdlib_jdk = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin_version}"
     val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
@@ -52,29 +68,25 @@ object Androidx {
     val lifecycle = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle_version}"
     val legacy = "androidx.legacy:legacy-support-v4:${Versions.legacy_version}"
     val lifecycle_compiler_kapt =
-        "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_version}"
+            "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle_version}"
     val ifecycle_reactivestreams =
-        "androidx.lifecycle:lifecycle-reactivestreams:${Versions.lifecycle_version}"
-    val lifecycle_common = "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle_version}"
-    val lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle_version}"
+            "androidx.lifecycle:lifecycle-reactivestreams:${Versions.lifecycle_version}"
+    val lifecycle_common =
+            "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle_version}"
+    val lifecycle_runtime =
+            "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle_version}"
     val lifecycle_livedata =
-        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}"
+            "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle_version}"
     val constraintlayout =
-        "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout_version}"
-    val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.nav_version}"
+            "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout_version}"
+    val navigation_fragment =
+            "androidx.navigation:navigation-fragment-ktx:${Versions.nav_version}"
     val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.nav_version}"
     val room_runtime = "androidx.room:room-runtime:${Versions.room_version}"
     val room_ktx = "androidx.room:room-ktx:${Versions.room_version}"
     val room_compiler_kapt = "androidx.room:room-compiler:${Versions.room_version}"
     val lifecycle_viewmodel =
-        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}"
-
-    val junit = "junit:junit:${Versions.test_junit}"
-    val runner = "androidx.test:runner:${Versions.test_runner}"
-    val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso}"
-    val mockito = "org.mockito:mockito-inline:${Versions.mockitoInline}"
-    val core_testing = "androidx.arch.core:core-testing:${Versions.test_arch_core}"
-    val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:${Versions.mockitoKotlinVersion}"
+            "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}"
 }
 
 object Material {
@@ -88,9 +100,10 @@ object Picasso {
 object Retrofit {
     val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofit2_version}"
     val okhttp3 = "com.squareup.okhttp3:okhttp:${Versions.okhttp3_version}"
-    val converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.converter_gson_version}"
+    val converter_gson =
+            "com.squareup.retrofit2:converter-gson:${Versions.converter_gson_version}"
     val logging_interceptor =
-        "com.squareup.okhttp3:logging-interceptor:${Versions.logging_interceptor_version}"
+            "com.squareup.okhttp3:logging-interceptor:${Versions.logging_interceptor_version}"
 }
 
 object Gson {
@@ -99,12 +112,14 @@ object Gson {
 
 object Coroutines {
     val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines_version}"
-    val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines_version}"
+    val android =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines_version}"
 }
 
 object Groupie {
     val groupie = "com.xwray:groupie:${Versions.groupie_version}"
-    val groupie_extension = "com.xwray:groupie-kotlin-android-extensions:${Versions.groupie_version}"
+    val groupie_extension =
+            "com.xwray:groupie-kotlin-android-extensions:${Versions.groupie_version}"
 }
 
 object Hilt {
@@ -113,3 +128,13 @@ object Hilt {
     val kapt_hilt_android = "com.google.dagger:hilt-android-compiler:${Versions.hilt_kapt}"
     val kapt_hilt_compiler = "androidx.hilt:hilt-compiler:${Versions.hilt_kapt_compiler}"
 }
+
+object TestLib {
+    val junit = "junit:junit:${Versions.test_junit}"
+    val runner = "androidx.test:runner:${Versions.test_runner}"
+    val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    val mockito = "org.mockito:mockito-inline:${Versions.mockitoInline}"
+    val core_testing = "androidx.arch.core:core-testing:${Versions.test_arch_core}"
+    val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:${Versions.mockitoKotlinVersion}"
+}
+
