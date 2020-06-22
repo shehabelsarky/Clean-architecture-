feae8692cd6772de56b782d51dbdfdf464e0847f
shehabelsarky <nnabil.c@channels.com.sa>
2020-06-22 15:03:36 +0200

    adding SplashActivity, adding quadrant ignore activity meta data

---
+++ popularpersons/src/main/java/com/example/popularpersons/ui/activity/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.popularpersons.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.popularpersons.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+const val THREE_SECONDS = 3000L
+class SplashActivity : AppCompatActivity() {
+    private val foreground = CoroutineScope(Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        foreground.launch {
+            delay(THREE_SECONDS)
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        }
+    }
+}
